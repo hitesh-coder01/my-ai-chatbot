a003febcfce467dff386bcf233726c0514cfbacf
hitesh-coder01 <hiteshpaliwal1996@gmail.com>
2026-03-28 16:40:46 +0530

    Refactor app.py to restore functionality

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import google.generativeai as genai
 import time
 
 # 1. SETUP: API Key yahan dhyan se paste karein
-import streamlit as st
-GOOGLE_API_KEY = st.secrets["AIzaSyCcRTv-Ygi814aUyM8nE_xmfOWq9iV-qEo"]
+GOOGLE_API_KEY = "AIzaSyCcRTv-Ygi814aUyM8nE_xmfOWq9iV-qEo"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 # 2. MODEL SELECTION: (Aapka working logic)
